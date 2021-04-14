4c6297f70c4fc26617034e8032707d1bb3d42efc
tafheem86 <tafheem8686@gmail.com>
2021-04-14 17:39:27 -0400

    first maven commit

---
+++ src/main/java/lilburn/App.java
@@ -0,0 +1,13 @@
+package lilburn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
